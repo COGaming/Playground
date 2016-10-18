87ab16f1b897f5f5714adbd6b68ae82525cbee66
olivierko <olivier@alldiksson.se>
2016-10-18 22:37:51 +0200

    movement basics

--- Playground/Game1.cs
+++ Playground/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Diagnostics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -10,7 +11,25 @@ namespace Playground
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        SpriteBatch _spriteBatch;
+        private Texture2D _player;
+        private Vector2 _playerPosition;
+
+        private float _floor = 350;
+        private float _maxLeft = 0;
+        private float _maxRight = 400;
+        private float _roof = 0;
+        private float _gravity = 100;
+        private float _walkSpeed = 50;
+        private float _fallSpeed = 70;
+        private float _moveSpeed = 70;
+        private float _jumpSpeed = 270;
+        private bool _isFalling = false;
+        private bool _isJumping = false;
+        private float _maxJumpHeight = 100;
+        private float _minJumpTime = 0.10f;
+        private float _maxJumpTime = 0.35f;
+        private float _currentJumpTime;
 
         public Game1()
         {
@@ -18,64 +37,118 @@ namespace Playground
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _player = new Texture2D(GraphicsDevice, 50, 50);
+
+            var data = new Color[50 * 50];
+
+            for (var i = 0; i < data.Length; ++i)
+            {
+                data[i] = Color.Chocolate;
+            }
+
+            _player.SetData(data);
+
+            _playerPosition = Vector2.Zero;
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        private void Move(Vector2 direction, GameTime gameTime)
+        {
+            var elapsedTotalSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            _isJumping = 
+                (direction.Y > 0 || (_currentJumpTime < _minJumpTime && _isJumping)) &&
+                (_currentJumpTime <= _maxJumpTime);
+
+            _isFalling = _playerPosition.Y < _floor && !_isJumping;
+
+            Debug.WriteLine(_currentJumpTime);
+
+            if (!_isJumping)
+            {
+                _currentJumpTime = 0f;
+                _isFalling = true;
+            }
+
+            if (_isJumping)
+            {
+                _playerPosition.Y -= (_jumpSpeed * direction.Y) * elapsedTotalSeconds;
+                _currentJumpTime += elapsedTotalSeconds;
+            }
+
+            if (_isFalling)
+            {
+                
+            }
+
+            _playerPosition.Y += _gravity * elapsedTotalSeconds;
+
+            if (direction.X != 0.0f)
+            {
+                _playerPosition.X += (_moveSpeed * direction.X) * elapsedTotalSeconds;
+            }
+
+            _playerPosition.Y = MathHelper.Min(_floor, _playerPosition.Y);
+        }
+
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
-            // TODO: Add your update logic here
+            var direction = Vector2.Zero;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                //_playerPosition.Y = MathHelper.Max(_roof, _playerPosition.Y - 1);
+                direction.Y = 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                //_playerPosition.X = MathHelper.Min(_maxRight, _playerPosition.X + 1);
+
+                direction.X = 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                //_playerPosition.X = MathHelper.Max(_maxLeft, _playerPosition.X - 1);
+
+                direction.X = -1;
+            }
+
+            Move(direction, gameTime);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(
+                sortMode: SpriteSortMode.BackToFront, 
+                blendState: BlendState.AlphaBlend, 
+                depthStencilState: DepthStencilState.Default
+            );
+
+            _spriteBatch.Draw(_player, _playerPosition, color: Color.Black);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
