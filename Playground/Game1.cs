e927201a1facbb1b3a6f66108d1fcc3ebe835fe8
Ravantti\christian <christian.ravanttti@gsu10.se>
2016-10-19 20:50:20 +0200

    Jump test

--- Playground/Game1.cs
+++ Playground/Game1.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -26,9 +27,13 @@ namespace Playground
         private float _jumpSpeed = 270;
         private bool _isFalling = false;
         private bool _isJumping = false;
+
         private float _maxJumpHeight = 100;
         private float _minJumpTime = 0.10f;
-        private float _maxJumpTime = 0.35f;
+        private float _maxJumpTime = 0.50f;
+        //This is how long the player should be jumping _minJumpTime will be added on this every time space is pressed untill it reaches _maxJumptime when _currentJumpTime is same or larger then this player starts to fall
+        private float _jumptimeToApply = 0.00f;
+
         private float _currentJumpTime;
 
         public Game1()
@@ -68,39 +73,60 @@ namespace Playground
         {
             var elapsedTotalSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            _isJumping = 
-                (direction.Y > 0 || (_currentJumpTime < _minJumpTime && _isJumping)) &&
-                (_currentJumpTime <= _maxJumpTime);
+            var jumpIsPressed = direction.Y > 0;
 
-            _isFalling = _playerPosition.Y < _floor && !_isJumping;
-
-            Debug.WriteLine(_currentJumpTime);
-
-            if (!_isJumping)
+            if (jumpIsPressed)
             {
-                _currentJumpTime = 0f;
+                 var wishedJumptime = _jumptimeToApply + _minJumpTime;
+                _jumptimeToApply = (wishedJumptime >= _maxJumpTime) ? _maxJumpTime : wishedJumptime;
+            }
+
+            _isJumping = (jumpIsPressed) || _isJumping;
+       
+            if (_isJumping && !_isFalling)
+            {
+                Jump(direction, elapsedTotalSeconds);
+            }
+            
+            MovePlayerPossX(direction, gameTime, elapsedTotalSeconds);
+            ApplyPlayerConstantGravity(elapsedTotalSeconds);
+            VerrifyPlayerNeverUnderGround(_isJumping);
+
+        }
+
+        private void Jump(Vector2 direction, Single elapsedTotalSeconds)
+        {
+            _playerPosition.Y -= (_jumpSpeed * direction.Y) * elapsedTotalSeconds;
+            _currentJumpTime += elapsedTotalSeconds;
+            if (_currentJumpTime >= _jumptimeToApply || _currentJumpTime >= _maxJumpTime)
+            {
+                _isJumping = false;
                 _isFalling = true;
-            }
+                _jumptimeToApply = 0;
+                _currentJumpTime = 0;
+            } 
+        }
 
-            if (_isJumping)
-            {
-                _playerPosition.Y -= (_jumpSpeed * direction.Y) * elapsedTotalSeconds;
-                _currentJumpTime += elapsedTotalSeconds;
-            }
+        private void VerrifyPlayerNeverUnderGround(bool isJumping)
+        {
+             _playerPosition.Y = MathHelper.Min(_floor, _playerPosition.Y);
+            if (!isJumping && _playerPosition.Y < _floor)
+                _isFalling = true;
+            if (_playerPosition.Y >= _floor)
+                _isFalling = false;
+        }
 
-            if (_isFalling)
-            {
-                
-            }
-
+        private void ApplyPlayerConstantGravity(Single elapsedTotalSeconds)
+        {
             _playerPosition.Y += _gravity * elapsedTotalSeconds;
+        }
 
+        private void MovePlayerPossX(Vector2 direction, GameTime gameTime, Single elapsedTotalSeconds)
+        {
             if (direction.X != 0.0f)
             {
                 _playerPosition.X += (_moveSpeed * direction.X) * elapsedTotalSeconds;
             }
-
-            _playerPosition.Y = MathHelper.Min(_floor, _playerPosition.Y);
         }
 
         protected override void Update(GameTime gameTime)
